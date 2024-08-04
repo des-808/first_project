5676f8ec3f58441c19a437fd0c1ac2bdb0300d4c
Sergey <94394415+des-808@users.noreply.github.com>
2024-08-04 19:36:23 +0300

    попытка закомитить

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-import BookLib.Author;
-import BookLib.Book;
-import BookLib.Genre;
-import BookLib.Publisher;
+import models.Author;
+import models.Book;
+import models.Genre;
+import models.Publisher;
+import utils.Library;
+
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,7 +14,7 @@ public class Main {
         Library databaseLibrary = new Library();
         boolean exit = false;
         while (!exit) {
-            for (String s : Arrays.asList( "\nОнлайн Библиотека\n", "Меню: ", "1. Добавить книгу", "2. Удалить книгу", "3. Найти книгу по ISBN", "4. Найти книгу по автору", "5. Изменить книгу", "6. Показать все книги","7. Удаление базы данных", "8. Выход", "Выберите пункт меню: ")) {
+            for (String s : Arrays.asList( "\nОнлайн Библиотека\n", "Меню: ", "1. Добавить книгу", "2. Удалить книгу", "3. Найти книгу по ISBN", "4. Найти книгу по автору", "5. Найти книгу по Издателю", "6. Найти книгу по Жанру", "7. Изменить книгу", "8. Показать все книги","9. Удаление базы данных", "10. Выход", "Выберите пункт меню: ")) {
                 System.out.println(s);
             }
             int choice = scanner.nextInt();
@@ -23,8 +25,10 @@ public class Main {
                     String isbn = scanner.nextLine();
                     System.out.print("Введите название книги: ");
                     String title = scanner.nextLine();
-                    System.out.print("Введите полное имя автора: ");
-                    String authorr = scanner.nextLine();
+                    System.out.print("Введите Фамилию автора: ");
+                    String authorlastName = scanner.nextLine();
+                    System.out.print("Введите Имя автора: ");
+                    String authorFirstName = scanner.nextLine();
                     System.out.print("Введите имя Издательства: ");
                     String publisher = scanner.nextLine();
                     System.out.print("Введите Жанр книги: ");
@@ -33,7 +37,7 @@ public class Main {
                     double price = Double.parseDouble(scanner.nextLine());
                     System.out.print("Введите колличество страниц книги: ");
                     int pages = Integer.parseInt(scanner.nextLine());
-                    Author newAuthor  = new Author(authorr);
+                    Author newAuthor  = new Author(authorFirstName,authorlastName);
                     Publisher newPublisher = new Publisher(publisher);
                     Genre newGenre  = new Genre(genre);
                     Book newBook = new Book(title,price,pages,isbn,newAuthor,newPublisher,newGenre);
@@ -62,26 +66,77 @@ public class Main {
                     System.out.println(bookToFind != null ? bookToFind : "Книга не найдена");
                     break;
                 case 4:
-                    System.out.println("Введите полное имя автора: ");
-                    String authorToFind = scanner.nextLine();
-                    var booksByAuthor = databaseLibrary.findBookByAuthor(authorToFind);
-                    for (var book : booksByAuthor) {
+                    System.out.println("Введите Имя автора ");
+                    String authorToFindFirstName= scanner.nextLine();
+                    System.out.println("Введите Фамилию автора ");
+                    String authorToFindLastName= scanner.nextLine();
+                    Author authorToFind = new Author(authorToFindFirstName, authorToFindLastName);
+                    for(var book : databaseLibrary.findBookByAuthor(authorToFind)) {
                         System.out.println(book);
                     }
                     break;
                 case 5:
-                    System.out.println("Введите ISBN книги для изменения: ");
+                    System.out.println("Введите Имя издателя ");
+                    String publisherToFindName= scanner.nextLine();
+                    Publisher publisherToFind = new Publisher(publisherToFindName);
+                    for(var book : databaseLibrary.findBookByPublisher(publisherToFind)) {
+                        System.out.println(book);
+                    }
+                    break;
+                case 6:
+                    System.out.println("Введите Жанр книги ");
+                    String genreToFindName= scanner.nextLine();
+                    Genre genreToFind = new Genre(genreToFindName);
+                    for(var book : databaseLibrary.findBookByGenre(genreToFind)) {
+                        System.out.println(book);
+                    }
+                    break;
+                case 7:
+                System.out.println("Введите ISBN книги для изменения: ");
                     String isbnToUpdate = scanner.nextLine();
                     Book bookToUpdate = databaseLibrary.findBookByIsbn(isbnToUpdate);
                     if(bookToUpdate != null) {
+                        System.out.println("Книга: "+bookToUpdate .getTitle());
                         System.out.print("Введите новое название книги: ");
                         String newTitle = scanner.nextLine();
-                        //System.out.print("Введите новое полное имя автора: ");
-                        //author = scanner.nextLine();
-                        //newAuthor.setName(scanner.nextLine());
+                        //------------------------------------------------------
+                        System.out.println("Цена книги: " + bookToUpdate.getPrice());
+                        System.out.print("Введите новое Цену книги: ");
+                        double newPrice = Double.parseDouble(scanner.nextLine());
+                        //------------------------------------------------------
+                        System.out.println("Страниц книги: : " + bookToUpdate.getPages());
+                        System.out.print("Введите новое колличество страниц книги: ");
+                        int newPages = Integer.parseInt(scanner.nextLine());
+                        //------------------------------------------------------
 
+                        Author findAu = databaseLibrary.findAuthorId(bookToUpdate.getAuthorId());
+                        System.out.println("Фамилия автора: " + findAu.getLastName());
+                        System.out.print("Введите новую Фамилию автора: ");
+                        String newAuthorLastName = scanner.nextLine();
+                        System.out.println("Имя автора: " + findAu.getFirstName());
+                        System.out.print("Введите новое Имя автора: ");
+                        String newAuthorFirstName = scanner.nextLine();
+                        //------------------------------------------------------
+                        Publisher findPub = databaseLibrary.findPublisherId(bookToUpdate.getPublisherId());
+                        System.out.println("Имя Издательства: " + findPub.getName());
+                        System.out.print("Введите новое имя Издательства: ");
+                        String newPublisherName = scanner.nextLine();
+                        //------------------------------------------------------
+                        Genre findGenre = databaseLibrary.findGenreId(bookToUpdate.getGenreId());
+                        System.out.println("Жанр книги: " + findGenre.getName());
+                        System.out.print("Введите новый Жанр книги: ");
+                        String newGenreName = scanner.nextLine();
+                        //------------------------------------------------------
+                        Author newAuthorr = new Author(newAuthorFirstName, newAuthorLastName);
+                        Publisher newPublisherr = new Publisher(newPublisherName);
+                        Genre newGenr = new Genre(newGenreName);
                         bookToUpdate.setTitle(newTitle);
-                        //bookToUpdate.setAuthor(newAuthor);
+                        bookToUpdate.setPrice(newPrice);
+                        bookToUpdate.setPages(newPages);
+                        bookToUpdate.setAuthor(newAuthorr);
+                        bookToUpdate.setPublisher(newPublisherr);
+                        bookToUpdate.setGenre(newGenr);
+
                         if(databaseLibrary.updateBook(bookToUpdate)) {
                             System.out.println("Книга успешно изменена");
                         }
@@ -93,18 +148,18 @@ public class Main {
                         System.out.println("Книга не найдена");
                     }
                     break;
-                case 6:
+                case 8:
                     System.out.printf("Всего найдено %d книг в библиотеке\n", databaseLibrary.listBooks().size());
                     for(var book : databaseLibrary.listBooks()) {
                         System.out.println(book);
                     }
                     break;
-                case 7:
+                case 9:
                     System.out.print("Введите название базы данных для удаления: ");
                     //String dbName = scanner.nextLine();
-                    DatabaseManager.dropDatabase(scanner.nextLine());
+                    //utils.DatabaseManager.dropDatabase(scanner.nextLine());
                     break;
-                case 8:
+                case 10:
                     exit = true;
                     break;
                 default:
